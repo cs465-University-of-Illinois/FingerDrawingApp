a5a0ea53d29f52bb57fee94f8f0a24d99bbc9db4
Brian Bailey <bpbailey@illinois.edu>
2017-11-10 23:56:41 -0600

    Finger drawing app

---
+++ app/src/main/java/edu/illinois/cs465/fingerdrawingapp/MainActivity.java
@@ -0,0 +1,37 @@
+package edu.illinois.cs465.fingerdrawingapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_menu);
+        setActionBar(toolbar);
+
+        ColorSelectionInteraction colorButton = new ColorSelectionInteraction(this);
+        WidthSelectionInteraction wButton = new WidthSelectionInteraction(this);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_overflow_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.erase_view_menu_item) {
+            DrawingView view = (DrawingView) findViewById(R.id.drawing_view);
+            if (view != null) {
+                view.eraseContent();
+            }
+        }
+        return true;
+    }
+}
